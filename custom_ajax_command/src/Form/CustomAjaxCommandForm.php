6dbf4c74ef15efda96df5add980e859a2fa81464
kowaraj <kowaraj4stuff@gmail.com>
2019-06-27 19:12:14 +0200

    Add renaming oldest screenshot to the current one

--- custom_ajax_command/src/Form/CustomAjaxCommandForm.php
+++ custom_ajax_command/src/Form/CustomAjaxCommandForm.php
@@ -18,6 +18,20 @@ class CustomAjaxCommandForm extends FormBase {
 		return $this->t('v.0.1.99');
 	}
 
+	public function setLastToBeCurrent() {
+		$dir_path = '/drupal/sites/test-apashnin.web.cern.ch/files/EventDisplay/buffer/';
+		$d = array_diff(scandir($dir_path), array('..', '.', '.DAV'));
+		$fn_last = end($d); 
+		$fn_first = array_pop(array_reverse($d));
+		$ss_current_fn = '/drupal/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_current.png';
+		//unlink($copy_dest_fn);
+		if (! rename($dir_path . $fn_first, $ss_current_fn)) {
+		 	return 'rename failed';
+		} else {
+			return $dir_path . " : " . $fn_first . ' -> ' . $fn_last;
+		}
+	}
+
 	public function getListOfScreenshots() {
 		$dir_path = '/drupal/sites/test-apashnin.web.cern.ch/files/EventDisplay/buffer/';
 		$d = array_diff(scandir($dir_path), array('..', '.', '.DAV'));
@@ -27,6 +41,7 @@ class CustomAjaxCommandForm extends FormBase {
 	public function buildForm(array $form, FormStateInterface $form_state) {
 		$title2 = $this->getTitle2();
 		$list_of_files = $this->getListOfScreenshots();
+		$fn_last = $this->setLastToBeCurrent();
 
 		$form['offline_ed'] = [
 			'#type' => 'checkbox',
@@ -43,6 +58,7 @@ class CustomAjaxCommandForm extends FormBase {
 		];
 		$form['textfields_container']['textfields'] = [
 			'#type' => 'fieldset',
+			'#title' => $fn_last,
 			// '#title' => $this->t("this is a title of the container"),
 			// '#description' => t('this is the content of the container'),
 		];
@@ -58,7 +74,7 @@ class CustomAjaxCommandForm extends FormBase {
 		} else {
 			$form['textfields_container']['textfields']['online_ed'] = [
 				'#theme' => 'ed_online',
-				'#ss_path' => '/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_default.png',
+				'#ss_path' => '/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_current.png',
 			];			
 		}
 		return $form;
