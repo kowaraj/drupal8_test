cac54b13fe47cb89b2af11caae09c07cbdfda24a
kowaraj <kowaraj4stuff@gmail.com>
2019-06-27 17:10:34 +0200

    Add a switch for online-offline Event Display

--- custom_ajax_command/src/Form/CustomAjaxCommandForm.php
+++ custom_ajax_command/src/Form/CustomAjaxCommandForm.php
@@ -14,49 +14,53 @@ class CustomAjaxCommandForm extends FormBase {
 		return 'custom_ajax_command_form';
 	}
 	
+	public function getTitle2() {
+		return $this->t('v.0.1.99');
+	}
+
+	public function getListOfScreenshots() {
+		$dir_path = '/drupal/sites/test-apashnin.web.cern.ch/files/EventDisplay/buffer/';
+		$d = array_diff(scandir($dir_path), array('..', '.', '.DAV'));
+		return $d; 
+	}
+
 	public function buildForm(array $form, FormStateInterface $form_state) {
-		$form['message'] = [
-			'#type' => 'markup',
-			'#markup' => '<div class="result_message">TEST-DIV-RES-MES</div>',
-		];
-		$form['number_1'] = [
-			'#type' => 'textfield',
-			'#title' => $this->t('Number 1'),
-		];
-		$form['number_2'] = [
-			'#type' => 'textfield',
-			'#title' => $this->t('Number 2'),
-		];
-		$form['actions'] = [
-			'#type' => 'button',
-			'#value' => $this->t('Submit'),
+		$title2 = $this->getTitle2();
+		$list_of_files = $this->getListOfScreenshots();
+
+		$form['offline_ed'] = [
+			'#type' => 'checkbox',
+			'#title' => $this->t('Go \'offline\' to explore stored events.'),
 			'#ajax' => [
-				'callback' => '::setMessage',
+			  'callback' => '::textfieldsCallback',
+			  'wrapper' => 'textfields-container',
+			  'effect' => 'fade',
 			],
+		  ];
+		$form['textfields_container'] = [
+			'#type' => 'container',
+			'#attributes' => ['id' => 'textfields-container'],
 		];
-		$form['ed_template_test'] = [
-			'#theme' => 'my_template2',
-			'#test_var' => $this->t('Test Value for the Form!'),
-		];
-		$form['ed_template3'] = [
-			'#theme' => 'my_template3',
-			'#test_var' => $this->t('Test Value for the Form for Template #3!'),
-			'#ss_path' => '/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_default.png',
-			'#events' => ['event1', 'event2'],
-            '#featured' => ['featured1', 'featured2'],
-
-		];
-		$form['ed_ss'] = [
-			'#type' => 'markup',
-			'#markup' => '			
-			<div>
-			<p>
-			<img id="ed00" style="width:200px;height:120px;border:0;" src="/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_default.png" />
-			</p>
-			</div>
-			',
+		$form['textfields_container']['textfields'] = [
+			'#type' => 'fieldset',
+			// '#title' => $this->t("this is a title of the container"),
+			// '#description' => t('this is the content of the container'),
 		];
 
+		if ($form_state->getValue('offline_ed', NULL) === 1) {
+			$form['textfields_container']['textfields']['offline_ed'] = [
+				'#theme' => 'my_template3',
+				'#test_var' => $title2,
+				'#ss_path' => '/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_default.png',
+				'#events' => $list_of_files,
+				'#featured' => ['featured1', 'featured2'],
+			];
+		} else {
+			$form['textfields_container']['textfields']['online_ed'] = [
+				'#theme' => 'ed_online',
+				'#ss_path' => '/sites/test-apashnin.web.cern.ch/files/EventDisplay/ss_default.png',
+			];			
+		}
 		return $form;
 	}
 
@@ -74,5 +78,9 @@ class CustomAjaxCommandForm extends FormBase {
 	
 	public function submitForm(array &$form, FormStateInterface $form_state) {
 	}
+	
+	public function textfieldsCallback($form, FormStateInterface $form_state) {
+		return $form['textfields_container'];
+	  }
 	
 }
